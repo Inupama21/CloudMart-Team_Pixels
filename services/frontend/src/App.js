f51087684d4dbd480d50d639d7fad00bf495eea2
kulathungadmdt <kulathungadmdt.21@uom.lk>
2026-06-12 00:51:35 +0530

    adde p tag testing 3

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -228,7 +228,7 @@ function App() {
         <div style={styles.logo} onClick={() => setPage('products')}>
           CloudMart
         </div>
-        <p>By-Team_Pixels</p>
+        <p>ByTeamPixels</p>
         <nav style={styles.nav}>
           <button style={styles.navBtn} onClick={() => setPage('products')}>Products</button>
           <button style={styles.navBtn} onClick={() => setPage('cart')}>
