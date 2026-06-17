602a22b6ffa44734c5846d996580a937fa4c6e44
kulathungadmdt <kulathungadmdt.21@uom.lk>
2026-06-17 22:36:16 +0530

    testing p tag staging 12

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -228,7 +228,7 @@ function App() {
         <div style={styles.logo} onClick={() => setPage('products')}>
           CloudMart
         </div>
-        <p>By - Hello world1200</p>
+        <p>By - Elden Ring</p>
         <nav style={styles.nav}>
           <button style={styles.navBtn} onClick={() => setPage('products')}>Products</button>
           <button style={styles.navBtn} onClick={() => setPage('cart')}>
